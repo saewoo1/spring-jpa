5884703e539a5258e2d489d26fc425fa06bcd43b
saewoo1 <wooozooo1123@icloud.com>
2023-12-02 15:31:19 +0900

    feat: MemberRepository 설정

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    /*
+     * EntityManager 통한 모든 변경들은
+     * transaction 안에서 이루어져야 한다!!
+     * 추가적으로 Transactional 어노테이션이 테스트 클래스 안에 있으면,
+     * 테스트 종료 후 모든 데이터를 롤백한다.
+     * 롤백하기시르믄 Rollback(false) 추가 -> 커밋 해버림
+     * */
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void save() {
+        //given
+        Member member = new Member();
+        member.setUserName("saewoo");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+
+        /*
+         * 저장한 것 == 조회한 것
+         * 같은 영속성 컨텍스트(하나의 transaction) 안에서는
+         * id(식별자) 값이 같으면 같은 entity로 식별하기 때문
+         * */
+        assertThat(findMember).isEqualTo(member);
+    }
+}
